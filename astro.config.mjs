0c5fc023c413e93d185a8a93344b538fef12aa65
John Sherrell <dev@ohn.sh>
2026-03-06 01:28:41 -0600

    Tweak theme, add socials.

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,12 +14,18 @@ export default defineConfig({
         dark: '@/assets/days-dark.svg',
         light: '@/assets/days-light.svg',
       },
+      customCss: ['@/global.css'],
       head: [
         { tag: 'link', attrs: { rel: 'icon', href: '/favicon.ico', sizes: '32x32' } },
         { tag: 'link', attrs: { rel: 'icon', href: '/favicon.png', type: 'image/png' } },
       ],
       // favicon value always rendered after custom tags, so it needs to be the preferred icon.
       favicon: '/favicon.svg',
+      social: [
+        { icon: 'github', label: 'GitHub', href: 'https://github.com/ohnsh/scratch' },
+        { icon: 'youtube', label: 'YouTube', href: 'https://youtube.com/@ohn-sh' },
+        { icon: 'instagram', label: 'Instagram', href: 'https://instagram.com/j.ohn.sh' },
+      ],
       sidebar: [
         { label: 'March', autogenerate: { directory: '2026/mar' } },
         { label: 'February', autogenerate: { directory: '2026/feb' }, collapsed: true },
