4744f2ad8ddb61181381ddd18881018895d0816a
John Sherrell <dev@ohn.sh>
2026-03-09 12:53:22 -0500

    Fixed og:image bug on index page. routeData.ts improvements. Use content collection filtering API with `getCollection`. The index entry has id `index` when returned by `getCollection` but it's the empty string when accessed through `Astro.locals.starlightRoute`.

--- src/routeData.ts
+++ src/routeData.ts
@@ -1,17 +1,39 @@
 import { defineRouteMiddleware } from '@astrojs/starlight/route-data'
+import { getCollection, type CollectionEntry } from 'astro:content'
+import type { StarlightRouteData } from '@astrojs/starlight/route-data'
 
-export const onRequest = defineRouteMiddleware((context) => {
+function extractOgImage(entry: CollectionEntry<'docs'>) {
+  const { body, data } = entry
+  if (data.ogImage) {
+    return data.ogImage
+  }
+  const { videoId } = body?.match(/<YouTube\s+id="(?<videoId>[^"]+)"/)?.groups ?? {}
+  if (videoId) {
+    return `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`
+  }
+}
+
+function setOgImage(head: StarlightRouteData['head'], ogImage: string) {
+  head.push({ tag: 'meta', attrs: { property: 'og:image', content: ogImage } })
+}
+
+export const onRequest = defineRouteMiddleware(async (context) => {
   const { entry, head } = context.locals.starlightRoute
-  const { id, body, data } = entry
 
-  if (!data.ogImage) {
-    const { videoId } = body?.match(/<YouTube\s+id="(?<videoId>[^"]+)"/)?.groups ?? {}
-    if (videoId) {
-      data.ogImage = `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`
+  if (entry.id === 'index' || entry.id === '') {
+    const docs = await getCollection('docs', ({ id }) => id !== 'index')
+    docs.sort((a, b) => Number(b.data.date) - Number(a.data.date))
+    for (const doc of docs) {
+      const ogImage = extractOgImage(doc)
+      if (ogImage) {
+        setOgImage(head, ogImage)
+        break
+      }
+    }
+  } else {
+    const ogImage = extractOgImage(entry)
+    if (ogImage) {
+      setOgImage(head, ogImage)
     }
   }
-  if (!data.ogImage) {
-    return
-  }
-  head.push({ tag: 'meta', attrs: { property: 'og:image', content: data.ogImage } })
 })
