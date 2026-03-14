bce02d73f1eab01699a443aa8b013e8395064c94
John Sherrell <dev@ohn.sh>
2026-03-14 01:31:03 -0500

    Paginated /tags/[tag] route. As a bonus, og:image now works on tag pages. Both og:image and pagination links are implemented in route middleware, so any paginated routes get them for free.
    Realized that using [...page].astro instead of [page].astro allows for more elegant routes: the first page has no page segment (the `page` parameter is undefined) so there's no need for an index page returning `Astro.rewrite('/route/1')`. So now I'm using it on both paginated routes: /tags/[tag] and /latest.

--- src/routeData.ts
+++ src/routeData.ts
@@ -1,14 +1,5 @@
 import { defineRouteMiddleware, type StarlightRouteData } from '@astrojs/starlight/route-data'
-import { extractOgImage, setOgImage } from '@/lib/util'
-
-function ogImageFromPage(page) {
-  for (const post of page.data) {
-    const ogImage = extractOgImage(post)
-    if (ogImage) {
-      return ogImage
-    }
-  }
-}
+import type { DayEntry } from './lib/days'
 
 function getPagination(page): StarlightRouteData['pagination'] {
   const next = page.url.next ? { href: page.url.next } : undefined
@@ -18,14 +9,45 @@ function getPagination(page): StarlightRouteData['pagination'] {
 
 export const onRequest = defineRouteMiddleware(async (context) => {
   const { head, pagination } = context.locals.starlightRoute
-  const { page } = context.locals.days ?? {}
-
-  // const ogImage = page ? ogImageFromPage(page) : extractOgImage(entry)
-  // if (ogImage) {
-  //   setOgImage(head, ogImage)
-  // }
-
+  const { day, page } = context.locals.days ?? {}
+  
   if (page) {
+    const ogImage = ogImageFromPage(page)
+    if (ogImage) {
+      setOgImage(head, ogImage)
+    }
     Object.assign(pagination, getPagination(page))
+  } else if (day) {
+    const ogImage = extractOgImage(day)
+    if (ogImage) {
+      setOgImage(head, ogImage)
+    }
   }
 })
+
+function extractOgImage(entry: DayEntry) {
+  const { day, youtube } = entry
+  if (day?.data.ogImage) {
+    return day.data.ogImage
+  }
+  
+  const videoId = youtube?.data[0].videoId
+  // const { videoId } = body?.match(/<YouTube\s+id="(?<videoId>[^"]+)"/)?.groups ?? {}
+  
+  if (videoId) {
+    return `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`
+  }
+}
+
+function ogImageFromPage(page) {
+  for (const day of page.data) {
+    const ogImage = extractOgImage(day)
+    if (ogImage) {
+      return ogImage
+    }
+  }
+}
+
+function setOgImage(head: StarlightRouteData['head'], ogImage: string) {
+  head.push({ tag: 'meta', attrs: { property: 'og:image', content: ogImage } })
+}
