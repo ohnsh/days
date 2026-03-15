6f35a49b18c49170a7b96e9df80eb16a24329ba6
John Sherrell <dev@ohn.sh>
2026-03-14 23:54:21 -0500

    Added automatic tag extraction (based on emoji) to youtube loader. Using `Intl.Segmenter` API to handle complex multi-codepoint emoji. Also fixed some edge cases when parsing dates from video titles with implicit years. The year is taken from the pub date but adjusted as necessary in case the two dates fall on different sides of a December/January boundary.

--- src/loaders/youtube.ts
+++ src/loaders/youtube.ts
@@ -16,15 +16,25 @@ export function youtubeDays(): Loader {
           thumbnails,
           resourceId: { videoId },
         } = snippet
-        const dateId = getDateId(title, publishedAt)
+        const dayKey = getDayKey(title, publishedAt)
         const isShort = shorts.some(({ snippet }) => snippet.resourceId.videoId === videoId)
         if (isShort) {
           numShorts++
         }
-        if (!dayMap[dateId]) {
-          dayMap[dateId] = []
+        if (!dayMap[dayKey]) {
+          dayMap[dayKey] = []
         }
-        dayMap[dateId].push({ videoId, title, description, thumbnails, publishedAt, isShort })
+        const tags = tagsFromText(title, description)
+
+        dayMap[dayKey].push({
+          videoId,
+          title,
+          description,
+          thumbnails,
+          publishedAt,
+          isShort,
+          ...(tags.length > 0 ? { tags } : {}),
+        })
       }
 
       for (const [day, videos] of Object.entries(dayMap)) {
@@ -34,53 +44,88 @@ export function youtubeDays(): Loader {
   }
 }
 
-function getDateId(title: string, publishedAt: string) {
-  const matchDate = (string: string, pattern: RegExp) => {
-    for (const match of string.matchAll(pattern)) {
-      const date = new Date(match[0])
-      if (!Number.isNaN(date.valueOf())) {
-        return { date, match }
-      }
+function tagsFromText(title: string, description?: string) {
+  const tags = new Set
+  // Here's a new one. Even modern, unicode-safe iteration over strings will split complex emoji into several characters.
+  // (E.g. person, zero-width joiner, gender symbol.)
+  // `Intl.Segmenter` allows iteration over 'user-perceived' characters or graphemes.
+  const segmenter = new Intl.Segmenter('en', { granularity: 'grapheme' })
+  for (const { segment: char } of segmenter.segment(title)) {
+    if (char.length > 1) {
+      tags.add(char)
+    } else {
+      break
     }
   }
-  const dateFromTitle = (title: string, pubDate?: Date) => {
-    // TODO: some nasty time-zone edge cases to work through
-    const months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ]
-    const monthsPattern = months
-      .map((month) => month.slice(0, 3))
-      .concat(months)
-      .join('|')
-    const longDateTemplate = `(?<month>${monthsPattern})[.]? (?<day>\\d{1,2})\\b(, (?<year>\\d{4}))?`
-    const longDatePattern = new RegExp(longDateTemplate, 'ig')
-    const shortDatePattern = /(?<month>\d{1,2})\/(?<day>\d{1,2})\b(\/(?<year>\d{4}))?/g
+  return [...tags]
+}
 
-    const { date, match } =
-      matchDate(title, shortDatePattern) ?? matchDate(title, longDatePattern) ?? {}
-    if (!match || !date) {
-      return
-    }
-
-    if (!match.groups?.year && pubDate) {
-      // no explitic year in title so use the one from published date
-      date.setFullYear(pubDate.getFullYear())
-    }
-    return date
-  }
-
+function getDayKey(title: string, publishedAt: string) {
   const pubDate = new Date(publishedAt)
-  const dateId = (dateFromTitle(title, pubDate) ?? pubDate).toLocaleDateString('en-CA') // YYYY-mm-dd
-  return dateId
+  return (dateFromTitle(title, pubDate) ?? pubDate).toLocaleDateString('en-CA') // YYYY-mm-dd
+}
+
+function dateFromTitle(title: string, pubDate?: Date) {
+  const longDateTemplate = `(?<month>${monthsPattern()})[.]? (?<day>\\d{1,2})\\b(, (?<year>\\d{4}))?`
+  const longDatePattern = new RegExp(longDateTemplate, 'ig')
+  const shortDatePattern = /(?<month>\d{1,2})\/(?<day>\d{1,2})\b(\/(?<year>\d{4}))?/g
+
+  const { date, match } =
+    matchDate(title, shortDatePattern) ?? matchDate(title, longDatePattern) ?? {}
+  if (!match || !date) {
+    return
+  }
+
+  if (!match.groups?.year && pubDate) {
+    // no explitic year in title so use the one from published date
+    date.setFullYear(computeImplicitYear(date, pubDate))
+  }
+  return date
+}
+
+function monthsPattern() {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+  return months
+    .map((month) => month.slice(0, 3))
+    .concat(months)
+    .join('|')
+}
+
+function matchDate(string: string, pattern: RegExp) {
+  for (const match of string.matchAll(pattern)) {
+    const date = new Date(match[0])
+    if (!Number.isNaN(date.valueOf())) {
+      return { date, match }
+    }
+  }
+}
+
+// pubDate year might be off by one relative to implicit year (December/January or January/December).
+// Simply find which year makes the date closest to the pubDate.
+function computeImplicitYear(date: Date, refDate: Date) {
+  const baseYear = refDate.getFullYear()
+  const years = [baseYear - 1, baseYear, baseYear + 1]
+  const [[implicitYear]] = years
+    .map((year) => {
+      const d = new Date(date)
+      d.setFullYear(year)
+      return [year, Math.abs(refDate.getTime() - d.getTime())]
+    })
+    .sort(([, a], [, b]) => a - b)
+
+  // console.log({ date, refDate, implicitYear })
+  return implicitYear
 }
