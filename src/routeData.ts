0662d38cce1e535e51506c3fe045c672e0a9f923
John Sherrell <dev@ohn.sh>
2026-03-15 12:34:05 -0500

    Updated og:image extraction to extract multiple thumbnails if available. (E.g. Facebook lets you pick between them when posting.) Currently filtering out timelapse videos and prioritizing running videos.

--- src/routeData.ts
+++ src/routeData.ts
@@ -1,7 +1,8 @@
 import { defineRouteMiddleware, type StarlightRouteData } from '@astrojs/starlight/route-data'
 import type { DayEntry } from './lib/days'
+import type { Page } from 'astro'
 
-function getPagination(page): StarlightRouteData['pagination'] {
+function getPagination(page: Page): StarlightRouteData['pagination'] {
   const next = page.url.next ? { href: page.url.next } : undefined
   const prev = page.url.prev ? { href: page.url.prev } : undefined
   return { next, prev }
@@ -10,44 +11,48 @@ function getPagination(page): StarlightRouteData['pagination'] {
 export const onRequest = defineRouteMiddleware(async (context) => {
   const { head, pagination } = context.locals.starlightRoute
   const { day, page } = context.locals.days ?? {}
-  
+
   if (page) {
-    const ogImage = ogImageFromPage(page)
-    if (ogImage) {
-      setOgImage(head, ogImage)
-    }
+    const ogImages = ogImagesFromPage(page)
+    appendOgImages(head, ...ogImages)
     Object.assign(pagination, getPagination(page))
   } else if (day) {
-    const ogImage = extractOgImage(day)
-    if (ogImage) {
-      setOgImage(head, ogImage)
-    }
+    const ogImages = extractOgImages(day)
+    appendOgImages(head, ...ogImages)
   }
 })
 
-function extractOgImage(entry: DayEntry) {
+function extractOgImages(entry: DayEntry) {
   const { day, youtube } = entry
   if (day?.data.ogImage) {
-    return day.data.ogImage
+    return [day.data.ogImage]
   }
-  
-  const videoId = youtube?.data[0].videoId
+
+  const videoIds =
+    youtube?.data
+      // for now: remove timelapses from consideration
+      .filter(({ tags = [] }) => !tags.includes('⏳'))
+      // for now: prioritize running vid thumbnails
+      .sort((a, b) => (a.tags?.includes('🏃‍♂️') ? -1 : 0) - (b.tags?.includes('🏃‍♂️') ? -1 : 0))
+      .map(({ videoId }) => videoId) ?? []
+  // const videoId = youtube?.data[0].videoId
   // const { videoId } = body?.match(/<YouTube\s+id="(?<videoId>[^"]+)"/)?.groups ?? {}
-  
-  if (videoId) {
-    return `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`
-  }
+
+  return videoIds.map((videoId) => `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`)
 }
 
-function ogImageFromPage(page) {
+function ogImagesFromPage(page: Page) {
   for (const day of page.data) {
-    const ogImage = extractOgImage(day)
-    if (ogImage) {
-      return ogImage
+    const ogImages = extractOgImages(day)
+    if (ogImages.length > 0) {
+      return ogImages
     }
   }
+  return []
 }
 
-function setOgImage(head: StarlightRouteData['head'], ogImage: string) {
-  head.push({ tag: 'meta', attrs: { property: 'og:image', content: ogImage } })
+function appendOgImages(head: StarlightRouteData['head'], ...ogImages: string[]) {
+  ogImages.forEach((ogImage) => {
+    head.push({ tag: 'meta', attrs: { property: 'og:image', content: ogImage } })
+  })
 }
