04f0615975cdb588ce7f9607d1a07e43f293cbb9
John Sherrell <dev@ohn.sh>
2026-03-15 21:26:41 -0500

    Youtube loader: Generalized fetch-all-pages abstraction around YouTube Data API calls.

--- src/loaders/youtube_dl.ts
+++ src/loaders/youtube_dl.ts
@@ -5,25 +5,12 @@ async function savePlaylists(forHandle: string) {
   const uploadsPlaylistId = await getUploadsPlaylistId(forHandle)
   const shortsPlaylistId = uploadsPlaylistId.replace(/^UU/, 'UUSH')
 
-  const uploads = await getPlaylistItems(uploadsPlaylistId)
-  const shorts = await getPlaylistItems(shortsPlaylistId)
+  const uploads = await getPlaylist(uploadsPlaylistId)
+  const shorts = await getPlaylist(shortsPlaylistId)
 
   await mkdir('.days/youtube', { recursive: true })
   await Bun.file('.days/youtube/uploads.json').write(JSON.stringify(uploads))
   await Bun.file('.days/youtube/shorts.json').write(JSON.stringify(shorts))
-}
-
-async function getPlaylistItems(playlistId: string) {
-  // https://developers.google.com/youtube/v3/docs/playlistItems#resource
-  const allItems = []
-  let pageToken = undefined
-  do {
-    const { nextPageToken, items } = await getPlaylist(playlistId, pageToken)
-    allItems.push(...items)
-    pageToken = nextPageToken
-  } while (pageToken)
-
-  return allItems
 }
 
 async function api(path: string, params: Record<string, string>) {
@@ -42,6 +29,19 @@ async function api(path: string, params: Record<string, string>) {
   return json
 }
 
+async function apiAllPages(path: string, params: Record<string, string>) {
+  const allItems = []
+  let pageToken = undefined
+  do {
+    const _params = { ...params, ...(pageToken ? { pageToken } : {}) }
+    const { nextPageToken, items } = await api(path, _params)
+    allItems.push(...items)
+    pageToken = nextPageToken
+  } while (pageToken)
+
+  return allItems
+}
+
 async function getUploadsPlaylistId(forHandle: string): Promise<string> {
   const params = { part: 'snippet,contentDetails,statistics', forHandle }
   const json = await api('/channels', params)
@@ -49,16 +49,13 @@ async function getUploadsPlaylistId(forHandle: string): Promise<string> {
   return channel.contentDetails.relatedPlaylists.uploads
 }
 
-async function getPlaylist(playlistId: string, pageToken?: string) {
-  // pageToken
+async function getPlaylist(playlistId: string) {
   const params = {
     part: 'snippet',
     playlistId,
     maxResults: '50',
-    ...(pageToken ? { pageToken } : {}),
   }
-  const json = await api('/playlistItems', params)
-  return json
+  return await apiAllPages('/playlistItems', params)
 }
 
 await savePlaylists('ohn-sh')
