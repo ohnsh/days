fbfebc9861ee79aa2a01ca101b829c7555d835d4
John Sherrell <dev@ohn.sh>
2026-03-19 13:56:34 -0500

    lib/dates.ts finally doesn't make me wince. Thoroughly renamed and refactored. Instead of expecting only plain dates, `dayFromDate()` and `slugFromDate()` work with either full timestamps (which are fine to implicitly convert to local time) or plain dates (which are important to represent the way they were originally typed out). You explicitly specify whether it's a `plainDate` using a second parameter (default false). If so, it's normalized to midnight UTC so that later string extraction knows to always use UTC getters.

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -1,54 +1,59 @@
-export function slugFromDate(date: Date | string) {
-  const { year, monthShort, day } = partsFromDate(date)
-  return `${year}/${monthShort.toLowerCase()}/${day}`
+// `dayFromDate()` and `slugFromDate()` work with either full timestamps (which are fine to
+//   implicitly convert to local time) or plain dates (which are important to represent the way
+//   they were originally typed out). You explicitly specify whether it's a `plainDate` using a
+//   second parameter (default false). If so, it's normalized to midnight UTC so that later
+//   string extraction knows to always use UTC getters.
+export function slugFromDate(date: Date | string, plainDate = false) {
+  if (plainDate) {
+    date = normalizePlainDate(date)
+  }
+  if (typeof date === 'string') {
+    date = new Date(date)
+  }
+  const { year, month, day } = plainDate
+    ? {
+        year: date.getUTCFullYear(),
+        month: date.toLocaleDateString('en-US', { month: 'short', timeZone: 'UTC' }),
+        day: date.getUTCDate(),
+      }
+    : {
+        year: date.getFullYear(),
+        month: date.toLocaleDateString('en-US', { month: 'short' }),
+        day: date.getDate(),
+      }
+
+  return `${year}/${month.toLowerCase()}/${day}`
 }
 
-export function keyFromDate(_date: Date | string) {
-  const { date, isUTC } = normalizeDate(_date)
-  return date.toLocaleDateString('en-CA', isUTC ? { timeZone: 'UTC' } : undefined)
+export function dayFromDate(date: Date | string, plainDate = false) {
+  if (plainDate) {
+    return normalizePlainDate(date).toLocaleDateString('en-CA', { timeZone: 'UTC' })
+  }
+  if (typeof date === 'string') {
+    date = new Date(date)
+  }
+  return date.toLocaleDateString('en-CA')
 }
 
-export function titleFromDayKey(dayKey: string) {
-  const { date, isUTC } = normalizeDate(dayKey)
+export function titleFromDay(day: string) {
+  const date = normalizePlainDate(day)
   const now = new Date()
   const sigYear = now.getFullYear() - 1
   if (
-    date.getFullYear() < sigYear ||
-    (date.getFullYear() === sigYear && date.getMonth() <= now.getMonth())
+    date.getUTCFullYear() < sigYear ||
+    (date.getUTCFullYear() === sigYear && date.getUTCMonth() <= now.getMonth())
   ) {
-    return date.toLocaleDateString('en-US', {
-      dateStyle: 'full',
-      ...(isUTC ? { timeZone: 'UTC' } : {}),
-    })
+    return date.toLocaleDateString('en-US', { dateStyle: 'full', timeZone: 'UTC' })
   }
   return date.toLocaleDateString('en-US', {
     weekday: 'long',
     month: 'long',
     day: 'numeric',
-    ...(isUTC ? { timeZone: 'UTC' } : {}),
+    timeZone: 'UTC',
   })
 }
 
-export function partsFromDate(_date: Date | string) {
-  const { date, isUTC } = normalizeDate(_date)
-  return isUTC
-    ? {
-        year: date.getUTCFullYear(),
-        month: date.getUTCMonth() + 1,
-        day: date.getUTCDate(),
-        monthShort: date.toLocaleDateString('en-US', { month: 'short', timeZone: 'UTC' }),
-        monthLong: date.toLocaleDateString('en-US', { month: 'long', timeZone: 'UTC' }),
-      }
-    : {
-        year: date.getFullYear(),
-        month: date.getMonth() + 1,
-        day: date.getDate(),
-        monthShort: date.toLocaleDateString('en-US', { month: 'short' }),
-        monthLong: date.toLocaleDateString('en-US', { month: 'long' }),
-      }
-}
-
-function normalizeDate(date: Date | string) {
+function normalizePlainDate(date: Date | string) {
   if (typeof date === 'string') {
     date = new Date(date)
   }
@@ -56,9 +61,9 @@ function normalizeDate(date: Date | string) {
   const isUTC = date.getUTCHours() === 0
   if (!isUTC && date.getHours() !== 0) {
     throw new Error(
-      'To ensure predictable behavior, a date slug must parse to midnight local or UTC time. Use a plain date like 2026-03-01 or 3/1/2026.'
+      'Dates passed to normalizePlainDate() must parse to midnight local or UTC time. Use a plain date like 2026-03-01 or 3/1/2026.'
     )
   }
 
-  return { date, isUTC }
+  return isUTC ? date : new Date(`${date.toLocaleDateString('en-CA')}T00:00:00Z`)
 }
