e4bfe579da91d3178ca60ebe8ebb2b6b00530f00
John Sherrell <dev@ohn.sh>
2026-03-27 14:40:29 -0500

    Github loader: Incrementally fetch commits using `since` query parameter.

--- src/loaders/github_dl.ts
+++ src/loaders/github_dl.ts
@@ -1,10 +1,18 @@
 import * as Bun from 'bun'
 import { mkdir } from 'node:fs/promises'
 
-async function api(url: URL | string) {
+async function api(url: URL | string, params?: Record<string, string>) {
   const headers = {
     Accept: 'application/vnd.github+json',
     Authorization: `Bearer ${import.meta.env.GITHUB_TOKEN}`,
+  }
+
+  if (params) {
+    const _url = new URL(url)
+    Object.entries(params).forEach(([key, val]) => {
+      _url.searchParams.set(key, val)
+    })
+    url = _url
   }
 
   const resp = await fetch(url, { headers })
@@ -15,12 +23,12 @@ async function api(url: URL | string) {
   return resp
 }
 
-async function apiAllPages(url: URL | string) {
+async function apiAllPages(url: URL | string, params?: Record<string, string>) {
   const nextPattern = /<(\S+)>; rel="next"/i
   const data = []
   let nextPage: string | undefined
   do {
-    const resp = await api(nextPage ?? url)
+    const resp = await api(nextPage ?? url, params)
     const json = await resp.json()
     data.push(...json)
     nextPage = resp.headers.get('link')?.match(nextPattern)?.[1]
@@ -29,22 +37,13 @@ async function apiAllPages(url: URL | string) {
   return data
 }
 
-// TODO: incremental fetch of commits
-async function repoLatest(repo: string) {
-  const json = await Bun.file(`.days/github/commits/${repo}.json`).json()
-  const [latestCommit] = json
-  const { date } = latestCommit.commit.author
-  // `since` is a query parameter to commits endpoint
-  const since = date
-}
-
 async function ghDownloader() {
   // ?sort=updated ?sort=pushed
   // ?since=timestamp only show repositories updated after the given time
   // 304 Not modified (?)
   await mkdir('.days/github/commits', { recursive: true })
 
-  const reposFetched = await apiAllPages('https://api.github.com/user/repos?sort=updated')
+  const reposFetched = await apiAllPages('https://api.github.com/user/repos?sort=pushed')
   const reposSaved = await Bun.file('.days/github/repos.json')
     .json()
     .catch((e) => [])
@@ -53,17 +52,36 @@ async function ghDownloader() {
     const { full_name, pushed_at } = repo
     const saved = reposSaved.find((repo) => repo.full_name === full_name)
 
-    if (!saved || isUpdated(pushed_at, saved.pushed_at)) {
-      // https://docs.github.com/en/rest/commits/commits
-      const { name, commits_url } = repo
-      const url = new URL(commits_url.replaceAll(/{[^}]+}/g, '')) /* ?author=ohnsh */
-      url.searchParams.set('per_page', '50')
+    if (saved && !isUpdated(pushed_at, saved.pushed_at)) {
+      // console.log(`${repo.name} not updated; skipping.`)
+      continue
+    }
 
-      const commits = await apiAllPages(url).then((commits) =>
+    const { name, commits_url } = repo
+    // https://docs.github.com/en/rest/commits/commits
+    const url = new URL(commits_url.replaceAll(/{[^}]+}/g, '')) /* ?author=ohnsh */
+    let commits
+    if (!saved) {
+      console.log(`${name} not saved; fetching all.`)
+      url.searchParams.set('per_page', '50')
+      commits = await apiAllPages(url).then((commits) =>
         commits.filter(({ commit }) => isMyCommit(commit))
       )
-      await Bun.file(`.days/github/commits/${name}.json`).write(JSON.stringify(commits))
+    } else {
+      const savedCommits = await Bun.file(`.days/github/commits/${name}.json`).json()
+      const [latestCommit] = savedCommits
+      const { date } = latestCommit.commit.author
+      url.searchParams.set('per_page', '10')
+      url.searchParams.set('since', date)
+      console.log(`${name} cache out of date; fetching new commits since ${date}`)
+      const newCommits = await apiAllPages(url).then((commits) =>
+        commits.filter(({ commit }) => isMyCommit(commit))
+      )
+      console.log(`${newCommits.length} new commits; ${savedCommits.length} saved commits.`)
+      commits = [...newCommits, ...savedCommits]
     }
+
+    await Bun.file(`.days/github/commits/${name}.json`).write(JSON.stringify(commits))
   }
   await Bun.file('.days/github/repos.json').write(JSON.stringify(reposFetched))
 }
