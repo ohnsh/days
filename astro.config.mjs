d336e7bd5e436d6834c246dd749cbf9f570b419d
John Sherrell <dev@ohn.sh>
2026-08-25 11:00:48 -0500

    New post about homebridge/homekit automation to capture sensor data on schedule. Created a bare-bones Slideshow.astro component that uses Alpine.js for reactive state. Installed Astro Alpine integration. Bumped dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,40 +5,40 @@ import flexoki from 'starlight-theme-flexoki'
 import rehypeExternalLinks from 'rehype-external-links'
 import { sidebarTemplate } from './src/lib/const'
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://days.ohn.sh',
-  integrations: [
-    starlight({
-      plugins: [flexoki({ accentColor: 'yellow' })],
-      title: 'days.',
-      description:
-        'A blog experiment emphasizing aggregation. Content from various sources (GitHub, YouTube, regular posts, etc.) is merged into a day-oriented feed and archive that the author can curate and customize.',
-      lastUpdated: false,
-      routeMiddleware: '@/routeData.ts',
-      logo: {
-        replacesTitle: false,
-        dark: '@/assets/days-dark.svg',
-        light: '@/assets/days-light.svg',
+  integrations: [starlight({
+    plugins: [flexoki({ accentColor: 'yellow' })],
+    title: 'days.',
+    description:
+      'A blog experiment emphasizing aggregation. Content from various sources (GitHub, YouTube, regular posts, etc.) is merged into a day-oriented feed and archive that the author can curate and customize.',
+    lastUpdated: false,
+    routeMiddleware: '@/routeData.ts',
+    logo: {
+      replacesTitle: false,
+      dark: '@/assets/days-dark.svg',
+      light: '@/assets/days-light.svg',
+    },
+    customCss: ['@/styles/global.css'],
+    head: [
+      { tag: 'link', attrs: { rel: 'icon', href: '/favicon.ico', sizes: '32x32' } },
+      {
+        tag: 'link',
+        attrs: { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', type: 'image/png' },
       },
-      customCss: ['@/styles/global.css'],
-      head: [
-        { tag: 'link', attrs: { rel: 'icon', href: '/favicon.ico', sizes: '32x32' } },
-        {
-          tag: 'link',
-          attrs: { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', type: 'image/png' },
-        },
-      ],
-      // favicon value always rendered after custom tags, so it needs to be the preferred icon.
-      favicon: '/favicon.svg',
-      social: [
-        { icon: 'github', label: 'GitHub', href: 'https://github.com/ohnsh/days' },
-        { icon: 'youtube', label: 'YouTube', href: 'https://youtube.com/@ohn-sh' },
-        { icon: 'instagram', label: 'Instagram', href: 'https://instagram.com/j.ohn.sh' },
-      ],
-      sidebar: sidebarTemplate,
-    }),
-  ],
+    ],
+    // favicon value always rendered after custom tags, so it needs to be the preferred icon.
+    favicon: '/favicon.svg',
+    social: [
+      { icon: 'github', label: 'GitHub', href: 'https://github.com/ohnsh/days' },
+      { icon: 'youtube', label: 'YouTube', href: 'https://youtube.com/@ohn-sh' },
+      { icon: 'instagram', label: 'Instagram', href: 'https://instagram.com/j.ohn.sh' },
+    ],
+    sidebar: sidebarTemplate,
+  }), alpinejs()],
   markdown: { rehypePlugins: [[rehypeExternalLinks, { target: '_blank', rel: [] }]] },
   // mdx({ rehypePlugins: [[rehypeExternalLinks, { target: '_blank', rel: [] }]] }),
   vite: {
